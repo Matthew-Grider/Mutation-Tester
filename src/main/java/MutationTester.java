f8fb862802bb74070298152d418209882ce192dc
MattGrider <mgride3@uic.edu>
2017-11-11 17:27:57 -0600

    working on tests

--- src/main/java/MutationTester.java
+++ src/main/java/MutationTester.java
@@ -1,13 +1,22 @@
 import javassist.bytecode.*;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+
+import java.io.File;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import javassist.*;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
 import static javassist.bytecode.Mnemonic.OPCODE;
 
 public class MutationTester {
@@ -78,6 +87,41 @@ public class MutationTester {
                 System.out.println("couldn't run mutation correctly : " + e);
             }
         }
+    }
+
+    public List<Result> runTest()
+    {
+        String[] temp = dirPath.split("production");
+        String testPath = temp[0] + "test" + temp[1];
+        List<Class<?>> run = new ArrayList<>();
+
+        File testDir = new File(testPath);
+
+        List<File> tests = new ArrayList<>(FileUtils.listFiles(testDir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE));
+
+        try {
+            URL[] classpath = {testDir.toURI().toURL()};
+            URLClassLoader urlcl = new URLClassLoader(classpath);
+
+            for(File test : tests) {
+                temp = test.getAbsolutePath().split("classes\\\\");
+                temp = (temp[1].replaceAll("\\\\",".")).split(".class");
+                run.add(urlcl.loadClass(temp[0]));
+            }
+        } catch(MalformedURLException e) {
+            System.out.println("Incorrect test path : " + e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Couldn't find class : " + e);
+        }
+
+        JUnitCore testRunner = new JUnitCore();
+        List<Result> results = new ArrayList<>();
+
+        for(Class<?> runnableTest : run) {
+            results.add(testRunner.run(runnableTest));
+        }
+
+        return results;
     }
 
     /*public static boolean modifyBytes(CtClass ct, Mutator mutation, java.lang.Class<?>... classes)
--- src/main/java/WorkThread.java
+++ src/main/java/WorkThread.java
@@ -1,12 +1,18 @@
+import org.junit.runner.Result;
+
 public class WorkThread implements Runnable {
     private MutationTester executer;
 
     public WorkThread(MutationTester newExecutter)
     {
-        executer = newExecutter;;
+        executer = newExecutter;
     }
 
     public void run(){
         executer.modifyBytes();
+        for(Result result : executer.runTest())
+        {
+            System.out.println(result.wasSuccessful());
+        }
     }
 }
